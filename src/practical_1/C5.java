d4527fdf1437bc4d5afe39bc3c6262745439223a
liaukx-tarc <liaukx-wm19@student.tarc.edu.my>
2021-07-03 00:50:59 +0800

    Done C5

--- src/practical_1/C5.java
+++ src/practical_1/C5.java
@@ -1,5 +1,5 @@
-
 package practical_1;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Stack;
@@ -7,20 +7,46 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class C5 {
+
     private Scanner userInput = new Scanner(System.in);
-   
+
+    static boolean result = true;
     String input;
-    private static Queue<Float> operandList = new LinkedList<>();
-    
+    private static Queue<Character> charQueue = new LinkedList<>();
+    private static Stack<Character> charStack = new Stack<Character>();
+
     public void enterString() {
         System.out.print("Enter String: ");
         input = userInput.nextLine();
     }
-    
-    public void checkPalindrome()
-    {
+
+    public void checkPalindrome() {
         for (int i = 0; i < input.length(); i++) {
-            
+            charStack.push(input.charAt(i));
+            charQueue.add(input.charAt(i));
+        }
+
+        do {
+            if (charStack.pop() != charQueue.poll()) {
+                result = false;
+            }
+
+        } while (charQueue.isEmpty() || charStack.empty());
+
+    }
+    
+     public static void main(String[] args) 
+    {
+        C5 scoreApp = new C5();
+        scoreApp.enterString();
+        scoreApp.checkPalindrome();
+        if(result == true)
+        {
+            System.out.println("It was Palindrome");
+        }
+        else
+        {
+            System.out.println("It not Palindrome");
         }
     }
 }
